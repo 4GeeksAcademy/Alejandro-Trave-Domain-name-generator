62ced198bd2d2ec722cb85a4a7113f70fd6fb7db
Alejandro Travé <121689156+Atravep@users.noreply.github.com>
2024-03-26 18:31:53 +0000

    wip

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("¡Vamos a generar nombres de dominios!");
+
+  let pronoun = ["El", "Nuestro", "Un"];
+  let adj = ["gran_comedor_de", "famoso_elaborador_de", "ladrón_de"];
+  let noun = ["morcones", "Pacatón", "cocidos"];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    let pronoun_part = array[index];
+    console.log(pronoun_part);
+  }
+
+  console.log();
 };
