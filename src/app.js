4b5e9c8edef4d40d9ee9c6da1747f5d818260a3f
Alejandro Travé <121689156+Atravep@users.noreply.github.com>
2024-03-27 16:55:52 +0000

    añado generador manual y domain hack

--- src/app.js
+++ src/app.js
@@ -5,18 +5,51 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+document.getElementById("generator").onclick = function() {
   //write your code here
   console.log("¡Vamos a generar nombres de dominios!");
+  document.getElementById("salida").classList.remove("alert-warning");
+  document.getElementById("salida").classList.add("alert-success");
 
-  let pronoun = ["El", "Nuestro", "Un"];
-  let adj = ["gran_comedor_de", "famoso_elaborador_de", "ladrón_de"];
-  let noun = ["morcones", "Pacatón", "cocidos"];
+  let pronoun = ["El_", "Nuestro_", "Un_"];
+  let adj = ["gran_comedor_de_", "famoso_elaborador_de_", "ladrón_de_"];
+  let noun = ["morcones", "pacatones", "cocidos"];
+  let ext = [".com", ".es", ".net"];
+  let resultHTML = "<table>";
 
-  for (let index = 0; index < pronoun.length; index++) {
-    let pronoun_part = array[index];
-    console.log(pronoun_part);
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let x = 0; x < noun.length; x++) {
+        for (let z = 0; z < ext.length; z++) {
+          if (noun[x].slice(-2) == "es" && ext[z] == ".es") {
+            noun[x] = noun[x].slice(0, -2);
+          }
+          let dominio = pronoun[i] + adj[y] + noun[x] + ext[z];
+          resultHTML += `<tr><td>${dominio}</td></tr>`;
+          noun = ["morcones", "pacatones", "cocidos"];
+
+          /*Para nombre aleatorio WIP
+          document.getElementById("generator").onclick = function() {
+            let pronoun_random =
+              pronoun[Math.floor(Math.random() * pronoun.length)];
+            let adj_random = adj[Math.floor(Math.random() * adj.length)];
+            let noun_random = noun[Math.floor(Math.random() * noun.length)];
+            let ext_random = ext[Math.floor(Math.random() * ext.length)];
+
+            let nuevoPronoun = pronoun_random + " ";
+            let nuevoAdj = adj_random + " ";
+            let nuevoNoun = noun_random + " ";
+            let nuevoExt = ext_random;
+            let resultado = document.querySelector("#result");
+            document.getElementById("salida").classList.remove("invisible");
+            document.getElementById("salida").classList.add("visible");
+            resultado.innerHTML =
+              nuevoPronoun + nuevoAdj + nuevoNoun + nuevoExt;
+          };*/
+        }
+      }
+    }
   }
-
-  console.log();
+  resultHTML += "</table>";
+  document.getElementById("salida").innerHTML = resultHTML;
 };
